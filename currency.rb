b17e76c1e362c5cd1a1b90efa48e76fbcff5d6b0
Ryan Graham <ryangrahamwpb@gmail.com>
2015-09-03 18:55:55 -0400

    first stage of currency class

---
+++ currency.rb
@@ -0,0 +1,24 @@
+class Currency
+
+  def initialize (amount,type)
+    @amount = amount
+    @type = type
+  end
+
+  def == (equal)
+    if @amount == equal.amount && @type == equal.type
+  end
+
+  #def amount
+    #@amount
+
+  #end
+
+  #def type
+    #@type
+  #end
+
+
+
+
+end
