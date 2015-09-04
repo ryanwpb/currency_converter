ba6498317583538139c57a61a11d452307657eba
Ryan Graham <ryangrahamwpb@gmail.com>
2015-09-04 11:11:16 -0400

    file now +, -, * currency

--- currency.rb
+++ currency.rb
@@ -1,13 +1,44 @@
 class Currency
+attr_accessor :amount, :type
 
-  def initialize (amount,type)
+  def initialize(amount,type)
     @amount = amount
     @type = type
   end
 
-  def == (equal)
+  def ==(equal)
     if @amount == equal.amount && @type == equal.type
+    else puts "DifferentCurrencyCodeError."
+    end
   end
+
+
+  def +(number)
+    if @type == number.type
+      Currency.new(@amount +number.amount,@type).amount
+      #@amount = @amount + amount
+    else puts "DifferentCurrencyCodeError."
+    end
+  end
+
+  def -(number)
+    if @type == number.type
+    Currency.new(@amount -number.amount,@type).amount
+    else puts "DifferentCurrencyCodeError."
+    end
+  end
+
+  def *(number)
+    if @type == number.type
+      Currency.new(@amount *number.amount,@type).amount
+    else  puts "DifferentCurrencyCodeError."
+    end
+  end
+
+
+
+end
+
 
   #def amount
     #@amount
@@ -16,9 +47,3 @@ class Currency
 
   #def type
     #@type
-  #end
-
-
-
-
-end
--- hwcode.rb
+++ hwcode.rb
@@ -7,5 +7,13 @@ new_dollar = Currency.new(10,"USD")
 
 
 puts dollar1.amount
+
 puts dollar2.type
+
+puts dollar1 + dollar2
+
+puts dollar1 - dollar2
+
 puts dollar1 == dollar2
+
+puts dollar1 * dollar2
